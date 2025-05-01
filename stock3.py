bda8e114bb5262cf522cf0f6597c217697047ea1
Priyanshu Holkar <prinhacker23@gmail.com>
2025-05-01 15:22:45 +0530

    Update stock3.py

--- stock3.py
+++ stock3.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 
@@ -36,7 +36,7 @@ if user_input:
         st.error("❌ No data found for this ticker. Please try a different one.")
     else:
         # Show data summary
-        st.subheader('Data from 2010 - 2019')
+        st.subheader(f'Data from {start} - {end}')
         st.write(df.describe())
 
         # Plot closing price
